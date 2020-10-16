23de8a9715c5ddb330397924fa1d113e5dabee58
Hiroshi Umehara <com@umehara.net>
2020-10-16 12:55:38 +0900

    add

---
+++ clipboard2PNG.bat
@@ -0,0 +1,21 @@
+@powershell -windowstyle hidden -NoProfile -ExecutionPolicy Unrestricted "&([ScriptBlock]::Create((cat -encoding utf8 \"%~f0\" | ? {$_.ReadCount -gt 2}) -join \"`n\"))" %*
+@exit /b
+# ↑ PowerShellスクリプトをbatにラッピングするためのおまじない
+
+Add-Type -AssemblyName System.Windows.Forms;
+[System.Drawing.Image]$clipboardImage = [Windows.Forms.Clipboard]::GetImage();
+
+if ($clipboardImage -eq $null)
+{
+  # クリップボード上に画像が無い場合はフルスクリーンをキャプチャ
+  [Windows.Forms.Sendkeys]::SendWait("{PrtSc}");
+  Start-Sleep -Milliseconds 250
+  $clipboardImage = [Windows.Forms.Clipboard]::GetImage();
+}
+
+if ($clipboardImage -ne $null)
+{
+  # 名前をつけて、batと同じ階層に保存
+  [String]$fileName = (Get-Date).ToString("yyyyMMdd_HHmmss") + ".png";
+  $clipboardImage.Save($fileName);
+}
